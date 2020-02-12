2a1713093bfa14b675d7e1ec8002ede6ceec07b0
ziggyzulu <k00083370@student.lit.ie>
2020-02-12 23:13:17 +0000

    First test successful

--- util.test.js
+++ util.test.js
@@ -1,3 +1,12 @@
  //Require the generate text function from the util file
  
  const { generateText} = require('./util');
+
+ test('should output name and age', () => {
+
+    const text = generateText('Max', 29);
+
+    expect(text).toBe('Max (29 years old)');
+
+ });
+
