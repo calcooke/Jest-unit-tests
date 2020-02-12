ece1172ae03a080a9d34bf2c32269dda251d2caa
ziggyzulu <k00083370@student.lit.ie>
2020-02-12 23:19:34 +0000

    Adding a second test case

--- util.test.js
+++ util.test.js
@@ -2,11 +2,23 @@
  
  const { generateText} = require('./util');
 
+ //Make sure it returns the valid response
+
  test('should output name and age', () => {
 
     const text = generateText('Max', 29);
 
     expect(text).toBe('Max (29 years old)');
+
+ });
+
+ //What happens if the user passes nothing into the function?
+ //The function should return undefined
+
+ test('Should output data-less text', () => {
+
+    const text = generateText();
+    expect(text).toBe('undefined (undefined years old)');
 
  });
 
